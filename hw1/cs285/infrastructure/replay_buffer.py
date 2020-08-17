9d2424a424993f9b12905c60772ae6e5eecaa287
Calvin Qiu <calvin_qiu@berkeley.edu>
2020-08-16 22:24:46 -0700

    BC done

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -58,7 +58,9 @@ class ReplayBuffer(object):
             self.terminals = np.concatenate([self.terminals, terminals])[-self.max_size:]
 
     ########################################
-    ########################################
+    ################
+
+    ########################
 
     def sample_random_data(self, batch_size):
         assert self.obs.shape[0] == self.acs.shape[0] == self.rews.shape[0] == self.next_obs.shape[0] == self.terminals.shape[0]
@@ -67,7 +69,9 @@ class ReplayBuffer(object):
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
-        return TODO, TODO, TODO, TODO, TODO
+        length = self.obs.shape[0]
+        indices = np.random.choice(length, batch_size, replace=False)
+        return self.obs[indices], self.acs[indices], self.rews[indices], self.next_obs[indices], self.terminals[indices]
 
     def sample_recent_data(self, batch_size=1):
         return self.obs[-batch_size:], self.acs[-batch_size:], self.rews[-batch_size:], self.next_obs[-batch_size:], self.terminals[-batch_size:]
--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -34,7 +34,7 @@ class MLPPolicy(BasePolicy):
             self.build_graph()
 
         # saver for policy variables that are not related to training
-        self.policy_vars = [v for v in tf.all_variables() if policy_scope in v.name and 'train' not in v.name]
+        self.policy_vars = [v for v in tf.global_variables() if policy_scope in v.name and 'train' not in v.name]
         self.policy_saver = tf.train.Saver(self.policy_vars, max_to_keep=None)
 
     ##################################
@@ -87,7 +87,7 @@ class MLPPolicy(BasePolicy):
         # HINT1: you will need to call self.sess.run
         # HINT2: the tensor we're interested in evaluating is self.sample_ac
         # HINT3: in order to run self.sample_ac, it will need observation fed into the feed_dict
-        return TODO
+        return self.sess.run(self.sample_ac, feed_dict={self.observations_pl :observation})
 
     # update/train this policy
     def update(self, observations, actions):
@@ -121,7 +121,7 @@ class MLPPolicySL(MLPPolicy):
         # TODO define the loss that will be used to train this policy
         # HINT1: remember that we are doing supervised learning
         # HINT2: use tf.losses.mean_squared_error
-        self.loss = TODO
+        self.loss = tf.losses.mean_squared_error(true_actions, predicted_actions)
         self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def update(self, observations, actions):
